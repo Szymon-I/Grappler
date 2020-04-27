ffe153bd05bfbe990e07e4c48b3744f86ff51615
pskrzypk <pawel-skrzypkowski@outlook.com>
2020-04-27 10:28:35 -0700

    glew added

--- install_libs.sh
+++ install_libs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt install -y  build-essential libgl1-mesa-dev libglu1-mesa-dev libxi-dev libXi-devel freeglut3-dev  glew-utils libglm-dev libsoil-dev
+sudo apt install -y  build-essential libgl1-mesa-dev libglu1-mesa-dev libxi-dev libXi-devel freeglut3-dev  glew-utils libglm-dev libsoil-dev libglew-dev
