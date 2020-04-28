b182e2779471805bfcfdbcfdffc7e39fa3300ff0
szymon <szymon.idziniak@gmail.com>
2020-04-28 19:29:26 +0200

    add context manager for pyser

--- pyser.py
+++ pyser.py
@@ -2,7 +2,8 @@ import serial
 import sys
 device_port = str(sys.argv[1])
 speed = int(sys.argv[2])
-ser = serial.Serial(device_port,speed)
+
 while True:
-    s = input()
-    ser.write(s.encode())
+    with serial.Serial(device_port,speed) as ser:
+        s = input()
+        ser.write(s.encode())
