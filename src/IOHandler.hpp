5020d51bc491e169ac2d10af79720027a094e04d
szymon <szymon.idziniak@gmail.com>
2020-04-30 00:34:02 +0200

    remove function declarations and variables for camera

--- src/IOHandler.hpp
+++ src/IOHandler.hpp
@@ -1,160 +1,81 @@
-
-// --------------------------------------------------------------
-// Variables for control the view
+#pragma once
 #include <GL/freeglut.h>
 #include <stdio.h>
+#include "main.hpp"
 
-#define VANISH_CHANGE 0.1
-#define DEFAULT_WINDOW_WIDTH 1920
-#define DEFAULT_WINDOW_HEIGHT 1080
+#define ESC_KEY 27
 
-int windowHeight = DEFAULT_WINDOW_HEIGHT, windowWidth = DEFAULT_WINDOW_WIDTH;
-
-GLfloat _scene_rotate_x = 0.0f;
-GLfloat _scene_rotate_y = 0.0f;
-GLfloat _scene_translate_x = 0.0f;
-GLfloat _scene_translate_y = 0.0f;
-GLfloat _scene_translate_z = -1.0f;
-
-// ------------ MOUSE VARIABLES ----------------------//
-
-// angle of rotation for the camera direction
-GLfloat angle = 0.0;
-// actual vector representing the camera’s direction
-GLfloat lx = 1.0f, lz = -1.0f, ly = 0.0f;
-// XZ position of the camera
-GLfloat xx = 0.0f, zz = -1.0f, yy = 1.0f;
-// mouse variables
-GLfloat mouseOnEdgeSpeed = 0.02, fraction = 0.2f;
-int oldMouseX = DEFAULT_WINDOW_WIDTH/2, oldMouseY = DEFAULT_WINDOW_HEIGHT/2;
+extern Camera camera;
 
 int _mouse_buttonState = GLUT_UP;
 int _mouse_buttonX, _mouse_buttonY;
-extern float vanishing;
 
 // --------------------------------------------------------------
-void SpecialKeys( int key, int x, int y )
+void SpecialKeys(int key, int x, int y)
 {
-    switch( key )
-    {
+	switch (key)
+	{
+	case GLUT_KEY_LEFT:
+		break;
 
-        case GLUT_KEY_LEFT:
-            //_scene_translate_x -= 0.1f;
-            break;
+	case GLUT_KEY_RIGHT:
+		break;
 
-        case GLUT_KEY_RIGHT:
-            //_scene_translate_x += 0.1f;
-            break;
+	case GLUT_KEY_UP:
+		break;
 
-        case GLUT_KEY_UP:
-            //_scene_translate_y += 0.1f;
-            break;
-
-        case GLUT_KEY_DOWN:
-            //_scene_translate_y -= 0.1f;
-            break;
-    }
-
-}
-void vanish_info(float x){
-	printf("Vanishing: %.1f\n",x);
+	case GLUT_KEY_DOWN:
+		break;
+	}
 }
 // --------------------------------------------------------------
-void Keyboard( unsigned char key, int x, int y )
+void Keyboard(unsigned char key, int x, int y)
 {
-	switch (key) {
-	case 'a':
-		xx -= cos(angle);
-		zz -= sin(angle);
+	switch (key)
+	{
+	case CAMERA_MOVE_LEFT_KEY:
+		camera.move_left();
 		break;
-	case 'd':
-		xx += cos(angle);
-		zz += sin(angle);
+	case CAMERA_MOVE_RIGHT_KEY:
+		camera.move_right();
 		break;
-	case 'w':
-		xx += lx * fraction;
-		zz += lz * fraction;
-		yy += ly * fraction;
+	case CAMERA_MOVE_FORWARD_KEY:
+		camera.move_forward();
 		break;
-	case 's':
-		xx -= lx * fraction;
-		zz -= lz * fraction;
-		yy -= ly * fraction;
+	case CAMERA_MOVE_BACK_KEY:
+		camera.move_back();
 		break;
-	case 27:
+	case ESC_KEY:
 		exit(0);
-		break;
 	}
 }
 
 // --------------------------------------------------------------
-void MouseButton( int button, int state, int x, int y )
+void MouseButton(int button, int state, int x, int y)
 {
 
-	if(button==3){
-		//_scene_translate_z += 0.1f;
+	if (button == 3)
+	{
+		camera.move_forward();
 	}
-	if(button==4){
-		//_scene_translate_z -= 0.1f;
+	if (button == 4)
+	{
+		camera.move_back();
 	}
 
-	if( button == GLUT_LEFT_BUTTON )
-    {
+	if (button == GLUT_LEFT_BUTTON)
+	{
 
-        _mouse_buttonState = state;
+		_mouse_buttonState = state;
 
-
-        if( state == GLUT_DOWN )
-        {
-            //_mouse_buttonX = x;
-            //_mouse_buttonY = y;
-        }
-    }
+		if (state == GLUT_DOWN)
+		{
+		}
+	}
 }
 
 // --------------------------------------------------------------
-void MouseMotion( int x, int y )
+void MouseMotion(int x, int y)
 {
-	angle += (x - oldMouseX) / 100.0;
-	ly -= (y - oldMouseY) / 100.0;
-
-	lx = sin(angle);
-	lz = -cos(angle);
-
-	oldMouseX = x;
-	oldMouseY = y;
-}
-
-// --------------------------------------------------------------
-void MouseWheel(int button, int dir, int x, int y)
-{
-    if (dir > 0)
-	{
-		xx += lx * fraction;
-		zz += lz * fraction;
-		yy += ly * fraction;
-	}
-	else
-	{
-		xx -= lx * fraction;
-		zz -= lz * fraction;
-		yy -= ly * fraction;
-	}
-}
-// --------------------------------------------------------------
-
-void MouseOnEdge(void)
-{
-	if (oldMouseX > windowWidth - 40)
-	{
-		angle += mouseOnEdgeSpeed;
-		lx = sin(angle);
-		lz = -cos(angle);
-	}
-	else if (oldMouseX < 40)
-	{
-		angle -= mouseOnEdgeSpeed;
-		lx = sin(angle);
-		lz = -cos(angle);
-	}
+	camera.move_on_mouse_movement(x, y);
 }
