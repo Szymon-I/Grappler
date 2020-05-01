7009cf191205e6960ec6d79328388e85adc191ef
szymon <szymon.idziniak@gmail.com>
2020-05-01 17:46:31 +0200

    add shader menu

--- src/Menu.hpp
+++ src/Menu.hpp
@@ -1,12 +1,24 @@
+#pragma once
+#include <algorithm>
+#define FRAGMENT_BASIC_PATH "./shaders/fragment.glsl"
+#define FRAGMENT_DIRECTIONAL_PATH "./shaders/fragment_directional.glsl"
+#define FRAGMENT_SPOTLIGHT_PATH "./shaders/fragment_spotlight.glsl"
 enum
 {
     CAMERA_OPTIONS,
+    SHADERS,
     EXIT,
 };
 enum
 {
     SERIAL_MONITOR_TURN_ON,
     SERIAL_MONITOR_TURN_OFF,
+};
+enum
+{
+    FRAGMENT_BASIC,
+    FRAGMENT_DIRECTIONAL,
+    FRAGMENT_SPOTLIGHT
 };
 
 void Menu(int value)
@@ -16,6 +28,13 @@ void Menu(int value)
     case EXIT:
         exit(0);
     }
+}
+void applyShaders(std::string shader_path)
+{
+    for (ProgramHandler program : AllPrograms)
+    {
+        program.update_fragment_shader(shader_path);
+    };
 }
 
 void CameraMenu(int value)
@@ -29,8 +48,28 @@ void CameraMenu(int value)
     case THIRD_PERSON:
         printf("THIRD_PERSON\n");
         camera.set_mode(THIRD_PERSON, grappler.get_position());
+        break;
     }
     glutPostRedisplay();
+}
+
+void ShaderMenu(int value)
+{
+    switch (value)
+    {
+    case FRAGMENT_BASIC:
+        printf("FRAGMENT_BASIC\n");
+        applyShaders(FRAGMENT_BASIC_PATH);
+        break;
+    case FRAGMENT_DIRECTIONAL:
+        printf("FRAGMENT_DIRECTIONAL\n");
+        applyShaders(FRAGMENT_DIRECTIONAL_PATH);
+        break;
+    case FRAGMENT_SPOTLIGHT:
+        printf("FRAGMENT_SPOTLIGHT\n");
+        applyShaders(FRAGMENT_SPOTLIGHT_PATH);
+        break;
+    }
 }
 
 void SerialMenu(int value)
@@ -57,9 +96,15 @@ void init_menu()
     glutAddMenuEntry("turn on monitor", SERIAL_MONITOR_TURN_ON);
     glutAddMenuEntry("turn off monitor", SERIAL_MONITOR_TURN_OFF);
 
+    int shader_menu = glutCreateMenu(ShaderMenu);
+    glutAddMenuEntry("fragment basic", FRAGMENT_BASIC);
+    glutAddMenuEntry("fragment directional", FRAGMENT_DIRECTIONAL);
+    glutAddMenuEntry("fragment spotlight", FRAGMENT_SPOTLIGHT);
+
     int main_menu = glutCreateMenu(Menu);
     glutAddSubMenu("change camera", camera_menu);
     glutAddSubMenu("serial", serial_menu);
+    glutAddSubMenu("shaders", shader_menu);
 
     glutAddMenuEntry("exit", EXIT);
     glutAttachMenu(GLUT_RIGHT_BUTTON);
