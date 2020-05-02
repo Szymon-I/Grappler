e7eeb78cea0d7eb2010a498e4aea6d67d0c0ecbd
szymon <szymon.idziniak@gmail.com>
2020-05-02 19:00:24 +0200

    refactor light class

--- src/Light.hpp
+++ src/Light.hpp
@@ -7,37 +7,39 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// max boundary for movement in z axis
 #define MAX_Z 10.0
 #define MIN_Z -10.0
 class Light{
 private:
-
+    // light parameters
     std::vector < glm::vec3 > Light_Ambient;
     std::vector < glm::vec3 > Light_Diffuse;
     std::vector < glm::vec3 > Light_Position;
 
+    // light movement speed
     float light_movement = 0.1;
 
 public:
-
+        // initialize Global Light object with all lights parameters as lists
         void init(std::vector < glm::vec3 > ambient, std::vector < glm::vec3 > diffuse, std::vector < glm::vec3 > position){
             this->Light_Ambient=ambient;
             this->Light_Diffuse=diffuse;
             this->Light_Position=position;
         }
-
+        // get ambient parameters
         std::vector < glm::vec3 > get_ambient(){
             return this->Light_Ambient;
         }
-
+        // get diffuse parameters
         std::vector < glm::vec3 > get_diffuse(){
             return this->Light_Diffuse;
         }
-
+        // get position parameters
         std::vector < glm::vec3 > get_position(){
             return this->Light_Position;
         }
-
+        // move light
         void move_light(int index)
         {
             if (Light_Position[index].z > MAX_Z || Light_Position[index].z < MIN_Z)
