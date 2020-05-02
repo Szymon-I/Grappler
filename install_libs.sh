92095b8cdbacda0200eb6ee2fb7434247e8b8c01
szymon <szymon.idziniak@gmail.com>
2020-05-02 17:14:18 +0200

    update required libs

--- install_libs.sh
+++ install_libs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# install libraries for linux
 sudo apt-get update -y
-sudo apt install -y  build-essential libgl1-mesa-dev libglu1-mesa-dev libxi-dev libXi-devel freeglut3-dev  glew-utils libglm-dev libsoil-dev libglew-dev
+sudo apt install -y  build-essential libgl1-mesa-dev libglu1-mesa-dev libxi-dev libXi-devel freeglut3-dev  glew-utils libglm-dev libsoil-dev libglew-dev minicom socat
+# install libraries for python
+pip install pyserial numpy
