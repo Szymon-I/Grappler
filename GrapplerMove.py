ae6a19a5a25332da0eabe3bc98dff1651a191de7
szymon <szymon.idziniak@gmail.com>
2020-05-03 15:57:20 +0200

    add vertical movement

--- GrapplerMove.py
+++ GrapplerMove.py
@@ -13,6 +13,7 @@ class GrapplerMove:
         self.device_port = str(sys.argv[1])
         self.speed = int(sys.argv[2])
         self.buffer = ''
+        self.y_pos = 0
 
     def show_menu(self):
         print('This program imitates grappler movement without hardware')
@@ -25,50 +26,57 @@ class GrapplerMove:
         print('s - move back (once)')
         print('a - move left (once)')
         print('d - move right (once)')
+        print('+ - move up (once)')
+        print('- - move down (once)')
 
     def line_movement(self, line_length=20):
         data = [
-            f'0.0/{GrapplerMove.normalizer}/0.0' for _ in range(line_length)]
+            f'0.0/{GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length)]
         data += [
-            f'0.0/{-GrapplerMove.normalizer}/0.0' for _ in range(line_length)]
+            f'0.0/{-GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length)]
         self.send_data(data)
 
     def square_movement(self, line_length=20):
         data = [
-            f'0.0/{GrapplerMove.normalizer}/0.0' for _ in range(line_length//2)]
-        data += [f'{+GrapplerMove.normalizer}/0.0/0.0' for _ in range(
+            f'0.0/{GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length//2)]
+        data += [f'{+GrapplerMove.normalizer}/0.0/{self.y_pos}' for _ in range(
             line_length//2)]
         data += [
-            f'0.0/{-GrapplerMove.normalizer}/0.0' for _ in range(line_length)]
+            f'0.0/{-GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length)]
         data += [
-            f'{-GrapplerMove.normalizer}/0.0/0.0' for _ in range(line_length)]
+            f'{-GrapplerMove.normalizer}/0.0/{self.y_pos}' for _ in range(line_length)]
         data += [
-            f'0.0/{GrapplerMove.normalizer}/0.0' for _ in range(line_length)]
-        data += [f'{GrapplerMove.normalizer}/0.0/0.0' for _ in range(
+            f'0.0/{GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length)]
+        data += [f'{GrapplerMove.normalizer}/0.0/{self.y_pos}' for _ in range(
             line_length//2)]
         data += [
-            f'0.0/{-GrapplerMove.normalizer}/0.0' for _ in range(line_length//2)]
+            f'0.0/{-GrapplerMove.normalizer}/{self.y_pos}' for _ in range(line_length//2)]
 
         self.send_data(data)
 
     def wave_movement(self, line_length=50):
         sin_arg = np.linspace(0, 2*math.pi, line_length)
         data = [
-            f'{math.sin(sin_arg[x])*GrapplerMove.normalizer}/{GrapplerMove.normalizer}/0.0' for x in range(line_length)]
+            f'{math.sin(sin_arg[x])*GrapplerMove.normalizer}/{GrapplerMove.normalizer}/{self.y_pos}' for x in range(line_length)]
         data += [
-            f'{-math.sin(sin_arg[x])*GrapplerMove.normalizer}/{-GrapplerMove.normalizer}/0.0' for x in range(line_length)]
+            f'{-math.sin(sin_arg[x])*GrapplerMove.normalizer}/{-GrapplerMove.normalizer}/{self.y_pos}' for x in range(line_length)]
         self.send_data(data)
 
     def move_once(self, direction):
         data = None
         if direction == 'w':
-            data = [f'0.0/{GrapplerMove.normalizer}/0.0']
+            data = [f'0.0/{GrapplerMove.normalizer}/{self.y_pos}']
         elif direction == 's':
-            data = [f'0.0/{-GrapplerMove.normalizer}/0.0']
+            data = [f'0.0/{-GrapplerMove.normalizer}/{self.y_pos}']
         elif direction == 'a':
-            data = [f'{GrapplerMove.normalizer}/0.0/0.0']
+            data = [f'{GrapplerMove.normalizer}/0.0/{self.y_pos}']
         elif direction == 'd':
-            data = [f'{-GrapplerMove.normalizer}/0.0/0.0']
+            data = [f'{-GrapplerMove.normalizer}/0.0/{self.y_pos}']
+        self.send_data(data)
+
+    def move_vert(self, abs_value):
+        self.y_pos += abs_value
+        data = [f'0.0/0.0/{self.y_pos}']
         self.send_data(data)
 
     def send_data(self, data, monitor=False):
@@ -92,6 +100,9 @@ class GrapplerMove:
                 self.wave_movement()
             elif option in ['w', 'a', 's', 'd']:
                 self.move_once(option)
+            elif option in ['+', '-']:
+                value = GrapplerMove.normalizer/10
+                self.move_vert(value if option == '+' else -value)
 
 
 if __name__ == "__main__":
