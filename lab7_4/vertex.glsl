72213b62833353b19bf85a2a61fcb5a88c375f3c
pskrzypk <pawel-skrzypkowski@outlook.com>
2020-05-09 12:02:45 -0700

    colibers added

---
+++ lab7_4/vertex.glsl
@@ -0,0 +1,21 @@
+#version 330
+
+layout( location = 0 ) in vec4 inPosition;
+layout( location = 1 ) in vec2 inUV;
+layout( location = 2 ) in vec3 inNormal;
+layout( location = 3 ) in mat4 matModelInst;
+
+uniform mat4 matProj;
+uniform mat4 matView;
+
+out vec4 inoutPos;
+out vec2 inoutUV;
+
+
+void main()
+{
+	gl_Position = matProj * matView * matModelInst * inPosition;
+
+	inoutPos = inPosition;
+	inoutUV = inUV;
+}
